6d861cbeaff61ef6edbfd8775c7c504ab80618f4
NaidaFatic <faticnaida@gmail.com>
2022-08-14 23:17:41 +0200

    env mongo

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://process.env.DB_NAME:process.env.DB_PASS@test.4wxap.mongodb.net/diary?retryWrites=true&w=majority"
+    url: process.env.URL
 };
--- index.js
+++ index.js
@@ -44,8 +44,7 @@ require("./routes/comments.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-const DB = process.env.DB_NAME;
-const DB_PASS = process.env.DB_PASS;
+const DB = process.env.URL;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
